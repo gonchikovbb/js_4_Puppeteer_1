203563196eec64d42924e080c7c9032c96b19cb2
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-08-03 11:22:43 +0700

    Add timeout = 60000 Jest

--- gh.test.js
+++ gh.test.js
@@ -13,29 +13,24 @@ describe("Github page tests", () => {
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1', {
-      timeout: 60000
-    });
+    await page.waitForSelector('h1');
     const title2 = await page.title();
     expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
-  });
+  }, timeout = 60000);
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href', {
-      timeout: 60000
-    }) );
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
     expect(actual).toEqual("#start-of-content");
-  });
+  }, timeout = 60000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
-      visible: true,
-      timeout: 60000
+      visible: true
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Sign up for free")
-  });
+  }, timeout = 60000);
 });
 
 test("The h1 should contain 'team'", async () => {
@@ -45,12 +40,10 @@ test("The h1 should contain 'team'", async () => {
 
   await page.waitForNavigation()
 
-  await page.waitForSelector("h1", {
-    timeout: 60000
-  });
+  await page.waitForSelector("h1");
   const title3 = await page.title();
   expect(title3).toEqual("GitHub for teams · Build like the best teams on the planet · GitHub");
-});
+}, timeout = 60000);
 
 test("The h1 should contain 'marketplace'", async () => {
   await page.goto("https://github.com");
@@ -59,24 +52,18 @@ test("The h1 should contain 'marketplace'", async () => {
 
   await page.waitForNavigation()
 
-  await page.waitForSelector("h1", {
-    timeout: 60000
-  });
+  await page.waitForSelector("h1");
   const title4 = await page.title();
   expect(title4).toEqual("GitHub Marketplace · to improve your workflow · GitHub");
-});
+}, timeout = 60000);
 
 test("The h1 should contain search", async () => {
   await page.goto("https://github.com");
   const searchLink = await page.$("[placeholder='Search GitHub']");
   await searchLink.type("JavaScript");
   await page.keyboard.press("Enter");
-  await page.waitForNavigation({
-    timeout: 60000
-  });
-  await page.waitForSelector("h2", {
-    timeout: 60000
-  });
+  await page.waitForNavigation();
+  await page.waitForSelector("h2");
   const title5 = await page.title();
   expect(title5).toEqual("Search · JavaScript · GitHub");
-});
+}, timeout = 60000);
